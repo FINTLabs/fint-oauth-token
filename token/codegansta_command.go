448f22cd26bcb3b23d3bf654cf0278ca7418da86
Frode Sjovatsen <frode@sjovatsen.net>
2017-06-25 17:47:48 +0200

    separated command and flags

--- token/codegansta_command.go
+++ token/codegansta_command.go
@@ -8,24 +8,28 @@ func GetTokenCommand() cli.Command {
 		Name:   "token",
 		Usage:  "get oauth2 token",
 		Action: CmdToken,
-		Flags: []cli.Flag{
-			cli.StringFlag{
-				Name:  "username",
-				Usage: "username",
-			},
-			cli.StringFlag{
-				Name:  "password",
-				Usage: "password",
+		Flags: 	GetTokenFlags(),
+	}
+}
 
-			},
-			cli.StringFlag{
-				Name:  "client_id",
-				Usage: "OAuth2 client id",
-			},
-			cli.StringFlag{
-				Name:  "client_secret",
-				Usage: "OAuth2 client secret",
-			},
+func GetTokenFlags() []cli.Flag {
+	return []cli.Flag{
+		cli.StringFlag{
+			Name:  "username",
+			Usage: "username",
+		},
+		cli.StringFlag{
+			Name:  "password",
+			Usage: "password",
+
+		},
+		cli.StringFlag{
+			Name:  "client_id",
+			Usage: "OAuth2 client id",
+		},
+		cli.StringFlag{
+			Name:  "client_secret",
+			Usage: "OAuth2 client secret",
 		},
 	}
 }
